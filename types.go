ff190106919891ff93cda717698272917cd52d53
Gabri432 <gabrielegatti432@gmail.com>
2022-08-30 12:32:34 +0200

    Using comments to describe custom types.

--- types.go
+++ types.go
@@ -1,9 +1,13 @@
 package laplace
 
+// MatRow type is simply a set of floating point numbers.
+//
+// Each number is ideally a section of a column.
 type MatRow struct {
 	Columns []float64
 }
 
+// Matrix type is a set of Matrows.
 type Matrix struct {
 	Rows []MatRow
 }
